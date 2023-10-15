d033221c1d92ec1bc66f05bf764cfec97b2c9412
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-10-15 20:06:35 +0530

    ENV CHANGES

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://iectxtycitvmqgpihdgy.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
