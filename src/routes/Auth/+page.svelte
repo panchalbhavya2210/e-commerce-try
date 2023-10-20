ccfeeebbd9236a2f48c13e4f278e5a39b3a8d322
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-10-21 00:06:52 +0530

    d

--- src/routes/Auth/+page.svelte
+++ src/routes/Auth/+page.svelte
@@ -49,6 +49,27 @@
     }
     // fetchData();
   });
+  let emailVal = "panchalbhavya2210@protonmail.com";
+
+  // function checkRegex() {
+  //   let regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+  //   console.log(emailVal);
+
+  //   if(emailVal)
+  // }
+  const validateEmail = (email) => {
+    let us = String(email)
+      .toLowerCase()
+      .match(
+        /\b[A-Za-z0-9._%+-]+@(gmail\.com|outlook\.com|yahoo\.com|protonmail\.com)\b/
+      );
+
+    if (us == null) {
+      alert("Please Enter Correct Format");
+    } else {
+      alert("Done");
+    }
+  };
 </script>
 
 <main>
@@ -60,7 +81,48 @@
     </div>
 
     <div class="formContainer">
-      <!--  -->
+      <div class="firstTwo sm:flex justify-center">
+        <div class="inputF m-auto">
+          <label for="firstname">First Name</label>
+          <br />
+          <input
+            type="text"
+            name=""
+            id="firstname"
+            class="m-1 border-2 border-neutral-200 rounded-sm p-1 font-bold outline-black"
+          />
+        </div>
+        <br />
+        <div class="inputS">
+          <label for="secondname">Second Name</label>
+          <br />
+          <input
+            type="text"
+            name=""
+            id="secondname"
+            class="m-1 border-2 border-neutral-200 rounded-sm p-1 font-bold outline-black"
+          />
+        </div>
+      </div>
+
+      <div class="emailFiedl sm:flex justify-center">
+        <div class="inputS">
+          <label for="email">Your Email</label>
+          <br />
+          <input
+            type="text"
+            name=""
+            id="email"
+            bind:value={emailVal}
+            class="m-1 border-2 border-neutral-200 rounded-sm p-1 font-bold outline-black h-278"
+          />
+          <p class="text-xs text-gray-600">
+            Accepted Domains: <span class="font-bold"
+              >gmail.com, yahoo.com, outlook.com, protonmail.com</span
+            >
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </main>
