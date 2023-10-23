1df29de531aa68ca1fac14507eb599c00bd6236c
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-10-23 20:56:02 +0530

    form done adding stockist radio left

--- src/routes/Auth/+page.svelte
+++ src/routes/Auth/+page.svelte
@@ -5,19 +5,9 @@
   import { fade, fly } from "svelte/transition";
   import eyeOff from "../../lib/assets/eye-off.svg";
   import eyeOn from "../../lib/assets/eye-on.svg";
+  import { get } from "svelte/store";
 
-  let showText = true;
-  let eyeClick;
-  let textValue = "";
-  let passwordValue = "";
-  let inputType = "text";
-
-  function toggleInputType() {}
-  function changeType() {
-    inputType = inputType === "text" ? "password" : "text";
-    eyeClick = !eyeClick;
-    showText = !showText;
-  }
+  let files;
 
   const dataToInsert = [
     {
@@ -31,18 +21,25 @@
     },
   ];
 
-  async function insertData() {
-    const { data, error } = await supabase
-      .from("SellerData")
-      .upsert(dataToInsert);
+  /**
+  * ? This is a function to insert user data and also an function for getting user id 
+  * async function insertData() {
+    const { data, error } = supabase.from("SellerData").upsert(dataToInsert);
 
     if (error) {
       console.error("Error inserting data:", error);
     } else {
       console.log("Data inserted successfully:", data);
     }
+
+    const userData = supabase.auth.getUser();
+    const getUid = userData.then((response) => {
+      console.log(response.data.user.id);
+    });
   }
-  // insertData();
+  * todo add stockist radio label when selected data gets transferred in stockist and when not data pushes in user
+  insertData();
+  */
   onMount(() => {
     // insertData();
     async function fetchData() {
@@ -64,24 +61,30 @@
     }
     // fetchData();
   });
-  let emailVal = "panchalbhavya2210@protonmail.com";
-
-  // function checkRegex() {
-  //   let regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-  //   console.log(emailVal);
-
-  //   if(emailVal)
-  // }
+  let emailVal;
+  let us;
   const validateEmail = (email) => {
-    let us = String(email)
+    us = String(email)
       .toLowerCase()
       .match(
         /\b[A-Za-z0-9._%+-]+@(gmail\.com|outlook\.com|yahoo\.com|protonmail\.com)\b/
       );
-
-    return us;
   };
-  validateEmail(emailVal);
+
+  function submitData() {
+    validateEmail(emailVal);
+
+    if (emailVal != "" && us == null) {
+      console.log(1);
+    } else {
+      const { data, error } = supabase.storage
+        .from("avatars")
+        .upload(`${uid}/${files[0]}`, avatarFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+    }
+  }
 </script>
 
 <!--
@@ -106,36 +109,71 @@
   <body class="h-full">
   ```
 -->
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div class="flex min-h-full flex-col justify-center px-6 py-6 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img
       class="mx-auto h-10 w-auto"
-      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+      src="https://raw.githubusercontent.com/panchalbhavya2210/e-commerce-try/main/src/lib/Screenshot_2023-10-15_162702-transformed-removebg-preview.png"
       alt="Your Company"
     />
     <h2
-      class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      class="mt-2 text-center text-2xl font-medium leading-9 tracking-tight text-gray-900"
     >
       Sign in to your account
     </h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6">
+      <div class="flexData flex">
+        <div>
+          <label
+            for="fname"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Firstname</label
+          >
+          <div class="mt-1">
+            <input
+              id="fname"
+              name="fname"
+              type="text"
+              required
+              class="font-medium p-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            for="lname"
+            class="ml-1 block text-sm font-medium leading-6 text-gray-900"
+            >Lastname</label
+          >
+          <div class="mt-1">
+            <input
+              id="lname"
+              name="lname"
+              type="text"
+              required
+              class="font-medium ml-1 p-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </div>
       <div>
         <label
           for="email"
           class="block text-sm font-medium leading-6 text-gray-900"
           >Email address</label
         >
-        <div class="mt-2">
+        <div class="mt-1">
           <input
             id="email"
             name="email"
             type="email"
             autocomplete="email"
             required
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            bind:value={emailVal}
+            class="font-medium p-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
@@ -147,42 +185,94 @@
             class="block text-sm font-medium leading-6 text-gray-900"
             >Password</label
           >
-          <div class="text-sm">
-            <a
-              href="#"
-              class="font-semibold text-indigo-600 hover:text-indigo-500"
+          <div class="text-sm underline">
+            <a href="#" class="font-semibold text-gray-600 hover:text-black"
               >Forgot password?</a
             >
           </div>
         </div>
-        <div class="mt-2">
+        <div class="mt-1">
           <input
             id="password"
             name="password"
             type="password"
             autocomplete="current-password"
             required
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            class="font-medium p-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
           />
         </div>
+        <label
+          class="mt-3 block text-sm font-medium text-gray-800"
+          for="file_input">Upload file</label
+        >
+        <input
+          class="mt-1 w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          id="file_input"
+          type="file"
+          bind:files
+        />
       </div>
 
       <div>
         <button
           type="submit"
-          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          on:click={insertData}
+          class="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black hover:transition-all focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
           >Sign in</button
         >
       </div>
     </form>
 
     <p class="mt-10 text-center text-sm text-gray-500">
-      Not a member?
+      Already a member?
       <a
-        href="#"
-        class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-        >Start a 14 day free trial</a
+        href="/Login"
+        class="font-semibold leading-6 text-gray-700 hover:text-black underline"
+        >Login</a
       >
     </p>
+
+    <div
+      class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md w-96 max-w-sm fixed bottom-5"
+      role="alert"
+    >
+      <div class="flex">
+        <div class="py-1">
+          <svg
+            class="fill-current h-6 w-6 text-teal-500 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            ><path
+              d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
+            /></svg
+          >
+        </div>
+        <div>
+          <p class="font-bold">Our privacy policy has changed</p>
+          <p class="text-sm break-all">Make sure you know how</p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3 shadow-md max-w-sm w-96 fixed bottom-5"
+      role="alert"
+    >
+      <div class="flex">
+        <div class="py-1">
+          <svg
+            class="fill-current h-6 w-6 text-red-500 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            ><path
+              d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
+            /></svg
+          >
+        </div>
+        <div>
+          <p class="font-bold">Our privacy policy has changed</p>
+          <p class="text-sm break-all">Make sure you know how</p>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
