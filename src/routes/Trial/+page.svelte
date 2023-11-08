94de476bfbd6d1ef9806843d9def7989ec562d6d
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-11-08 21:51:34 +0530

    f

--- src/routes/Trial/+page.svelte
+++ src/routes/Trial/+page.svelte
@@ -3,11 +3,13 @@
   sgMail.setApiKey(import.meta.env.VITE_SENDGRID_API);
 
   const msg = {
-    to: "geraltofrivia8491@gmail.com", // Recipient's email address
-    from: "your_email@example.com", // Your email address
+    to: "Geraltofrivia8491@gmail.com", // Recipient's email address
+    from: "VortexDeath@protonmail.com", // Your email address
     subject: "Example Subject",
     text: "This is a test email.",
   };
+
+  console.log(sgMail);
 
   sgMail
     .send(msg)
